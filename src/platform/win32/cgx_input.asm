d459b8ad9d5ad9a49513f7c0b1da13b9a08e5830
gui castaldeli <guicastaldeli8@gmail.com>
2026-09-12 17:13:00 -0400

    Update

--- src/platform/win32/cgx_input.asm
+++ src/platform/win32/cgx_input.asm
@@ -23,4 +23,37 @@ section .text
 ; Output: eax = 0 (released) or 1 (pressed)
 ; --------------------------------------------
 _cgxWin32GetKey:
-    
+    movzx eax, byte [rel _cgxWin32State + CGXWin32State.keys + rcx]
+    ret
+
+; --------------------------------------------
+; _cgxWin32GetMouseX
+; Output: eax = mouse X in pixels (relative to window)
+; --------------------------------------------
+_cgxWin32GetMouseX:
+    mov eax, [rel _cgxWin32State + CGXWin32State.mouseX]
+    ret
+
+; --------------------------------------------
+; _cgxWin32GetMouseY
+; Output: eax = mouse Y in pixels
+; --------------------------------------------
+_cgxWin32GetMouseY:
+    mov eax, [rel _cgxWin32State + CGXWin32State.mouseY]
+    ret
+
+; --------------------------------------------
+; _cgxWin32GetMouseButton
+; Input: ecx = button index (0=left, 1=middle, 2=right)
+; Output: eax = 0 (released) or 1 (pressed)
+; --------------------------------------------
+_cgxWin32MouseButton:
+    cmp ecx, 3
+    jae .invalid
+    movzx eax, byte [rel _cgxWin32State + CGXWin32State.mouseButtons + rcx]
+    ret
+
+.invalid:
+    xor eax, eax
+    ret
+
