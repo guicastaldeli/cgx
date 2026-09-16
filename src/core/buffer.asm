30dc868043bcad265521e9080a48c02b84a7a7c3
gui castaldeli <guicastaldeli8@gmail.com>
2026-09-16 17:10:52 -0400

    updatee

--- src/core/buffer.asm
+++ src/core/buffer.asm
@@ -35,4 +35,358 @@ section .text
 ; Output: eax = 1, 0 fail
 ; --------------------------------------------
 _cgxCoreBufferInit:
-    
+    push rbp
+    mov rbp, rsp
+    sub rsp, 32
+
+    ; Allocate initial pool: INTIIAL_CAPACITY * CGXBuffer_size bytes
+    xor rcx, rcx
+    mov rdx, INITIAL_CAPACITY * CGXBuffer_size
+    mov r8d, MEM_COMMIT | MEM_RESERVE
+    mov r9d, PAGE_READWRITE
+    call VirtualAlloc
+    test rax, rax
+    jz .fail
+
+    mov [rel _cgxCoreState + CGXState.bufferPool], rax
+    mov dword [rel _cgxCoreState + CGXState.bufferCapacity], INTIAL_CAPACITY
+    mov dword [rel, _cgxCoreState + CGXState.bufferCount], 0
+    mov dword [rel, _cgxCoreState + CGXState.nextBufferId], 1
+
+    mov eax, 1
+    jmp .done
+.fail:
+    xor eax,
+
+.done:
+    mov rsp, rbp
+    pop rbp
+    ret
+
+; --------------------------------------------
+; _cgxCoreBufferCreate
+; Input: rcx = data ptr, rdx = size in bytes, 
+;        r8d = usage
+;        r9d = buffer type (VERTEX/INDEX)
+; Output: eax = buffer id (>0), or 0 on failure
+; --------------------------------------------
+_cgxCoreBufferCreate:
+    push rbp
+    mov rbp, rsp
+    push rbx
+    push r12
+    push r13
+    push r14
+    push r15
+    sub rsp, 32
+
+    mov r12, rcx        ; data ptr
+    mov r13, rdx        ; size
+    mov r14d, r8d       ; usage
+    mov r15, r9d        ; type
+
+    ; Find a free slot
+    mov rbx, [rel _cgxCoreState + CGXState.bufferPool]
+    mov ecx, [rel _cgxCoreState + CGXState.bufferCapacity]
+    xor eax, eax        ; index
+
+.findSlot:
+    cmp eax, ecx
+    jge .growPool
+
+    ; slot = pool + index * CGXBuffer_size
+    mov edx, eax
+    imul edx, CGXBuffer_size
+    mov rdi, rbx
+    add rdi, rdx
+
+    cmp byte [rdi + CGXBuffer.inUse], 0
+    je .slotFound
+    inc eax
+    jmp .findSlot
+.growPool:
+    jmp .fail
+.slotFound:
+    ; rdi = free slot
+    ; rbx = pool base
+    ; eax = slot index
+
+    ; Allocate data buffer
+    push rax            ; save slot index
+    push rdi            ; save slot ptr
+
+    xor rcx, rcx
+    mov rdx, r13        ; size
+    mov r8d, MEM_COMMIT | MEM_RESERVE
+    mov r9d, PAGE_READWRITE
+    call VirtualAlloc
+    test rax, rax
+    jz. alloc_fail
+
+    mov r8, rax         ; r8 = new data ptr
+
+    ; Copy data
+    mov rcx, r12        ; src
+    mov rdx, r8         ; dst
+    mov rdi, r8
+    mov rsi, r12
+    mov rcx, r13        ; count bytes
+    rep movsb
+
+    ; Restore slot ptr
+    pop rdi
+    pop rax
+
+    ; Fill slot
+    mov edx, [rel _cgxCoreState + CGXState.nextBufferId]
+    mov [rdi + CGXBuffer.id], edx
+    mov [rdi + CGXBuffer.data], r8
+    mov [rdi + CGXBuffer.size], r13
+    mov [rdi + CGXBuffer.usage], r14d
+    mov [rdi + CGXBuffer.type], r15d
+    mov byte [rdi + CGXBuffer.inUse], 1
+
+    ; Increment counter and count
+    inc dword [rel _cgxCoreState + CGXState.nextBufferId]
+    inc dword [rel _cgxCoreState + CGXState.bufferCount]
+
+    mov eax, edx       ; return id
+    jmp .done
+
+.alloc_fail:
+    pop rdi
+    pop rax
+    jmp .fail
+.fail:
+    xor eax, eax
+
+.done:
+    add rsp, 32
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rbx
+    pop rbp
+    ret
+
+; --------------------------------------------
+; _cgxCoreBufferDelete
+; Input: ecx = buffer id
+; Output: eax = 1, 0 fail
+; --------------------------------------------
+_cgxCoreBufferDelete:
+    push rbp
+    mov rbp, rsp
+    push rbx
+    push r12
+    sub rsp, 32
+
+    mov r12d, ecx       ; id to delete
+
+    mov rbx, [rel _cgxCoreState + CGXState.bufferPool]
+    mov ecx, [rel _cgxCoreState + CGXState.bufferCapacity]
+    xor eax, eax
+
+.scan:
+    cmp eax, ecx
+    jge .notFound
+
+    mov edx, eax
+    imul edx, CGXBuffer_size
+    mov rdi, rbx
+    add rdi, rdx
+
+    cmp byte [rdi + CGXBuffer.inUse], 0
+    je .next
+
+    cmp dword [rdi + CGXBuffer.id], r12d
+    je .found
+.next:
+    inc eax
+    jmp .scan
+.found:
+    ; Free the data
+    mov rcx, [rdi + CGXBuffer.data]
+    test rcx, rcx
+    jz .markFree
+
+    xor rdx, rdx
+    mov r8d, MEM_RELEASE
+    call VirtualFree
+.markFree:
+    mov byte [rdi + CGXBuffer.inUse], 0
+    mov qword [rdi + CGXBuffer.data], 0
+    dec dword [rel _cgxCoreState + CGXState.bufferCount]
+
+    mov eax, 1
+    jmp .done
+
+.notFound:
+    xor eax, eax
+
+.done:
+    add rsp, 32
+    pop r12
+    pop rbx
+    pop rbp
+    ret
+
+; --------------------------------------------
+; _cgxCoreBufferBind
+; Input: ecx = buffer id, edx = type (VERTEX/INDEX)
+; Output: eax = 1 ok, 0 fail
+; --------------------------------------------
+_cgxCoreBufferBind:
+    push rbp
+    mov rbp, rsp
+    push rbx
+    push r12
+    push r13
+    sub rsp, 32
+
+    mov r12d, ecx       ; id
+    mov r13d, edx       ; type
+
+    mov rbx, [rel _cgxCoreState + CGXState.bufferPool]
+    mov ecx, [rel _cgxCoreState + CGXState.bufferCapacity]
+    xor eax, eax
+
+.scan:
+    cmp eax, ecx
+    jge .notFound
+
+    mov edx, eax
+    imul edx, CGXBuffer_size
+    mov rdi, rbx
+    add rdi, rdx
+
+    cmp byte [rdi + CGXBuffer.inUse], 0
+    je .next
+
+    cmp dword [rdi + CGXBuffer.id], r12d
+    je .found
+.next:
+    inc eax
+    jmp .scan
+.found:
+    ; Store bound id in state
+    cmp r13d, CGX_BUFFER_INDEX
+    je .bindEBO
+
+    ; Bind VBO  
+    mov [rel _cgxCoreState + CGXState.boundVBO], r12d
+    mov eax, 1
+    jmp .done
+
+.bindEBO:
+    mov [rel _cgxCoreState + CGXState.boundEBO], r12d
+    mov eax, 1
+    jmp .done
+
+.notFound:
+    xor eax, eax
+
+.done:
+    add rsp, 32
+    pop r13
+    pop r12
+    pop rbx
+    pop rbp
+    ret
+
+; --------------------------------------------
+; _cgxCoreBufferGetData
+; Input: ecx = buffer id
+; Output: rax = data pointer, or 0 if not found...
+; --------------------------------------------
+_cgxCoreBufferGetData:
+    push rbp
+    mov rbp, rsp
+    push rbx
+    push r12
+    sub rsp, 32
+
+    mov r12d, ecx
+    mov rbx, [rel _cgxCoreState + CGXState.bufferPool]
+    mov ecx, [rel _cgxCoreState + CGXState.bufferCapacity]
+    xor eax, eax
+
+.scan:
+    cmp eax, ecx
+    jge .notFound
+
+    mov edx, eax
+    imul edx, CGXBuffer_size
+    mov rdi, rbx
+    add rdi, rdx
+
+    cmp byte [rdi + CGXBuffer.inUse], 0
+    je .next
+
+    cmp dword [rdi + CGXBuffer.id], r12d
+    je .found
+.next:
+    inc eax
+    jmp .scan
+
+.found:
+    mov rax, [rdi + CGXBuffer.data]
+    jmp .done
+.notFound:
+    xor eax, eax
+
+.done:
+    add rsp, 32
+    pop r12
+    pop rbx
+    pop rbp
+    ret
+
+; --------------------------------------------
+; _cgxCoreBufferGetSize
+; Input: ecx = buffer id
+; Output: rax = size in bytes, or 0 if not found...
+; --------------------------------------------
+_cgxCoreBufferGetSize:
+    push rbp
+    mov rbp, rsp
+    push rbx
+    push r12
+    sub rsp, 32
+
+    mov r12d, ecx
+    mov rbx, [rel _cgxCoreState + CGXState.bufferPool]
+    mov ecx, [rel _cgxCoreState + CGXState.bufferCapacity]
+    xor eax, eax
+
+.scan:
+    cmp eax, ecx
+    jge .notFound
+
+    mov edx, eax
+    imul edx, CGXBuffer_size
+    mov rdi, rbx
+    add rdi, rbx
+
+    cmp byte [rdi + CGXBuffer.inUse], 0
+    je .next
+
+    cmp dword [rdi + CGXBuffer.id], r12d
+    je .found
+.next:
+    inc eax
+    jmp .scan
+
+.found:
+    rax, [rdi + CGXBuffer.size]
+    jmp .done
+.notFound:
+    xor eax, eax
+
+.done:
+    add rsp, 32
+    pop r12
+    pop rbx
+    pop rbp
+    ret
