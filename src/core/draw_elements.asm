895913bfcf19880e69ee32ff302222273ca7dfcd
gui castaldeli <guicastaldeli8@gmail.com>
2026-09-17 17:35:59 -0400

    Draw elements added

---
+++ src/core/draw_elements.asm
@@ -0,0 +1,5 @@
+; ============================================
+; core/draw_elements.asm
+; CGXDrawElements implementation
+; Reads vertices via VAO and draws each as pixel (stub)
+; ============================================
